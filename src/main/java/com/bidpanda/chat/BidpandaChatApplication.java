6daba1bd3ed6b5b4b4f8c7989d279eeaa0d9c6e7
byunggwan-Kim <ktera1231@gmail.com>
2023-10-07 10:31:03 +0900

    init: project setting

---
+++ src/main/java/com/bidpanda/chat/BidpandaChatApplication.java
@@ -0,0 +1,13 @@
+package com.bidpanda.chat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BidpandaChatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BidpandaChatApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bidpanda/chat/BidpandaChatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bidpanda.chat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BidpandaChatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
